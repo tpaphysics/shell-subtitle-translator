afd363f57e4b9313f355ee9253a4b244614aa274
tpaphysics <physics.thiago@gmail.com>
2022-04-17 04:18:52 -0300

    feat: criação da animação para monitorar o tempo da tradução, criação do arquivo .gitignore

--- traduction.sh
+++ traduction.sh
@@ -1,20 +1,57 @@
 #/bin/bash
 
 # Diretórios
-TRANSLATE_OUT_DIR="pt-br_traduction"
-LEGENDS_DIR="en_legends"
+
+#Pasta com as legendas
+IN_DIR="legends"
+
+#Pasta com as legendas traduzidas
+OUT_DIR="traduction"
 
 # Idioma de tradução
 TRANSLATE_LANGUAGE="en:pt-br"
 
-ls ./$LEGENDS_DIR >arq_list.txt
+ls ./$IN_DIR >arq_list.txt
 
-if [ ! -d $TRANSLATE_OUT_DIR ]; then
-    mkdir $TRANSLATE_OUT_DIR
+if [ ! -d $IN_DIR ]; then
+    mkdir $IN_DIR
+    mv *.srt ./$IN_DIR/
+fi
+
+if [ ! -d $OUT_DIR ]; then
+    mkdir $OUT_DIR
 fi
 
 while read line; do
-    trans -b $TRANSLATE_LANGUAGE -i ./$LEGENDS_DIR/"$line" >./$TRANSLATE_OUT_DIR/"$line" &
+    trans -b $TRANSLATE_LANGUAGE -i ./$IN_DIR/"$line" --no-warn >./$OUT_DIR/"$line" &
 done <arq_list.txt
 
-echo "The end!"
+# Animação spin
+spinner() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
+
+status=0
+clear
+
+# Motitora os processos que contem .srt no nome
+while [ $status -eq 0 ]; do
+    pgrep -f .srt >/dev/null
+    status=$?
+    printf 'Translating... '
+    spinner &
+    sleep 5
+    kill "$!" # kill the spinner
+    printf '\n'
+    clear
+done
+
+# Messagem exibida quando todos os processos terminam
+echo "[*] Translation completed successfully!"
+exit 0
